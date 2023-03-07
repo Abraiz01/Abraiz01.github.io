519de835890fadc73cd121024ce4f56c54418f06
Abraiz01 <78402495+Abraiz01@users.noreply.github.com>
2023-03-07 17:14:50 +0400

    added till page 7

--- CommunicationsLab/comicWebsite/app.js
+++ CommunicationsLab/comicWebsite/app.js
@@ -4,6 +4,27 @@
 let height = window.innerHeight;
 let width = window.innerWidth;
 
+// /* --- PAGE 1 --- */
+
+// var tl1 = new TimelineMax({onUpdate:updatePercentage3});
+
+// tl1.from('#summon-pic', .5, {y:300, opacity: 0});
+// tl1.from('#quote4', .5, {x:200, opacity: 0});
+
+// const scene1 = new ScrollMagic.Scene({
+//   triggerElement: "#page-1",
+//             triggerHook: "onLeave",
+//             duration: "100%"
+// })
+//   .setPin("#page-3")
+//   .setTween(tl1)
+// 		.addTo(controller);
+
+
+// function updatePercentage3() {
+//   tl1.progress();
+// }
+
 /* --- PAGE 2 --- */
 /* working of GSAP and Scrollmagic animation libraries */ 
 // initializing timeline variable for the current page
@@ -19,7 +40,7 @@ const controller = new ScrollMagic.Controller();
 
 // creating tween instances and defining parameters for the initial state of the animated object,
 // its speed, timing, opacity, and direction of motion
-tl2.from('#idea-pic', 1, {x:-200, opacity: 0, ease: Power4.easeInOut}, "=-.5");
+tl2.from('#grassground', 1, {y:200, opacity: 0, ease: Power4.easeInOut}, "=-.5");
 tl2.from('#quote3', .5, {x:-200, opacity: 0})
 
 
@@ -48,8 +69,8 @@ function updatePercentage2() {
 
 var tl3 = new TimelineMax({onUpdate:updatePercentage3});
 
-tl3.from('#summon-pic', .5, {y:300, opacity: 0});
-tl3.from('#quote4', .5, {x:200, opacity: 0});
+tl3.from('#city-background', .5, {y:300, opacity: 0});
+tl3.from('#city1', .5, {y:200, opacity: 0});
 
 const scene3 = new ScrollMagic.Scene({
   triggerElement: "#page-3",
@@ -69,8 +90,8 @@ function updatePercentage3() {
 
 var tl4 = new TimelineMax({onUpdate:updatePercentage4});
 
-tl4.from('#reach-pic', .5, {y:300, opacity: 0});
-tl4.from('#quote5', .5, {y:-200, opacity: 0});
+tl4.from('#city-background2', .5, {y:300, opacity: 0});
+tl4.from('#city2', .5, {y:200, opacity: 0});
 
 const scene4 = new ScrollMagic.Scene({
   triggerElement: "#page-4",
@@ -88,11 +109,10 @@ function updatePercentage4() {
 
 /* --- PAGE 5 --- */
 
-var tl5 = new TimelineMax({onUpdate:updatePercentage4});
+var tl5 = new TimelineMax({onUpdate:updatePercentage5});
 
-tl5.from('#happy-pic', .5, {x:-100, opacity: 0});
-tl5.from('#excited-pic', .5, {x:100, opacity: 0});
-tl5.from('#quote6', .5, {y:100, opacity: 0});
+tl5.from('#city-background3', .5, {y:300, opacity: 0});
+tl5.from('#city3', .5, {y:200, opacity: 0});
 
 const scene5 = new ScrollMagic.Scene({
   triggerElement: "#page-5",
@@ -108,7 +128,55 @@ function updatePercentage5() {
   tl5.progress();
 }
 
+/* --- PAGE 6 --- */
+
+var tl6 = new TimelineMax({onUpdate:updatePercentage5});
+
+tl6.from('#city-background3', .5, {y:300, opacity: 0});
+tl6.from('#city3', .5, {y:200, opacity: 0});
+
+const scene6 = new ScrollMagic.Scene({
+  triggerElement: "#page-5",
+            triggerHook: "onLeave",
+            duration: "100%"
+})
+  .setPin("#page-5")
+  .setTween(tl6)
+		.addTo(controller);
+
+
+function updatePercentage5() {
+  tl6.progress();
+}
+
 // /* --- code for GSAP and ScrollMagic ends here --- */
+
+var image = document.getElementById("bottle-pic");
+// image.addEventListener("click", changeImage);
+
+var scrollButton = document.getElementById("scroll-button");
+
+
+// function changeImage() {
+//   var image = document.getElementById("bottle-pic");
+//   var quote1 = document.getElementById("quote1");
+//   var quote2 = document.getElementById("quote2");
+
+//   quote1.textContent = "";
+//   quote2.textContent = "Click to Throw";
+//   image.src = "photos/bottle-laying.png";
+//   image.style.height = "20vw";
+//   image.style.top = "40vh";
+//   image.style.left = "30vw";
+//   scrollButton.style.display = "inline";
+// }
+
+document.getElementById("bottle-pic").addEventListener("click", function() {
+  const target = document.querySelector("#placeholder-1");
+  target.scrollIntoView({ behavior: "smooth" });
+});
+
+
 
 
 
