687d944a4b0e02055d5a2574ba956e5dff7719f0
Abraiz01 <78402495+Abraiz01@users.noreply.github.com>
2023-04-12 01:30:53 +0400

    images added

--- CommunicationsLab/soundProject/script.js
+++ CommunicationsLab/soundProject/script.js
@@ -1,28 +1,45 @@
 let unlockButton = document.getElementById("unlock-button");
 let attendButton = document.getElementById("attend-button");
-var ringtone = document.getElementById("ringtone");
-// var audioScam = document.getElementById("audio-scam");
+let restartButton = document.getElementById("restart-button");
+let ringtone = document.getElementById("ringtone");
+let uaeCallImg = document.getElementById("uae-call");
+let uaeRingingImg = document.getElementById("phone-ringing");
+const audioScam = document.getElementById("audio-scam");
+const bankAudio = document.getElementById("bank-audio");
 
 let audio = new Audio("sounds/ringing.mp3");
 
-const audioScam = document.getElementById("audio-scam");
+
 audioScam.addEventListener("ended", function () {
   // Do something when the audio playback has ended
-  console.log("Audio ended");
-  alert("Purchase of AED 5000 on your account XXX748360 was successful. Available balance is 11.34 AED");
+  console.log("Scam Audio ended");
+  alert(
+    "Purchase of AED 5000 on your account XXX748360 was successful. Available balance is 11.34 AED"
+  );
   // this is to know when the user has clicked the ok button on the alert
-  setTimeout(function() {
-    console.log("okay clicked")
+  setTimeout(function () {
+    console.log("okay clicked");
+    bankAudio.play();
   }, 0);
 });
 
+bankAudio.addEventListener("ended", function () {
+    // Do something when the audio playback has ended
+    console.log("Bank Audio ended");
+    restartButton.style.display = "block";
+  });
+
 document.getElementById("unlock-button").addEventListener("click", function () {
   console.log("Unlock button clicked");
+  unlockButton.style.display = "none";
   ringtone.play();
 });
 
 document.getElementById("attend-button").addEventListener("click", function () {
   console.log("Attend button clicked");
   ringtone.pause();
+  attendButton.style.display = "none";
+  uaeRingingImg.style.display = "none";
+  uaeCallImg.style.display = "block";
   audioScam.play();
 });
